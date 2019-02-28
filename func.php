0d43b623cbbab8ef25db20bb11c644a07c172f45
Vian RD <vianixa34@gmail.com>
2019-02-28 15:14:19 +0700

    Add files via upload

---
+++ func.php
@@ -0,0 +1,129 @@
+<?php
+
+class func {
+
+    private $conn;
+
+    // constructor
+    function __construct() {
+        require_once 'DB_Connect.php';
+        // connecting to database
+        $db = new Db_Connect();
+        $this->conn = $db->connect();
+    }
+
+    // destructor
+    function __destruct() {
+        
+    }
+
+    public function getData($type)
+    {
+        $curl = curl_init();
+        curl_setopt_array($curl, array(
+            CURLOPT_URL => "http://api.islamhouse.com/v1/ZsCcWofw9MdkYGT1/main/$type/id/id/1/25/json",
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_CUSTOMREQUEST => "GET",
+        ));
+
+        $response = curl_exec($curl);
+        $err = curl_error($curl);
+
+        curl_close($curl);
+
+        if($err)
+        {
+            $result = "error";
+            return $result;
+        }else{
+            return json_encode($response);
+        }
+    }
+
+    public function storeUser($name, $email, $password) {
+        $uuid = uniqid('', true);
+        $hash = $this->hashSSHA($password);
+        $encrypted_password = $hash["encrypted"]; // encrypted password
+        $salt = $hash["salt"]; // salt
+
+        $stmt = $this->conn->prepare("INSERT INTO users(unique_id, name, email, encrypted_password, salt, created_at) VALUES(?, ?, ?, ?, ?, NOW())");
+        $stmt->bind_param("sssss", $uuid, $name, $email, $encrypted_password, $salt);
+        $result = $stmt->execute();
+        $stmt->close();
+
+        if ($result) {
+            $stmt = $this->conn->prepare("SELECT * FROM users WHERE email = ?");
+            $stmt->bind_param("s", $email);
+            $stmt->execute();
+            $user = $stmt->get_result()->fetch_assoc();
+            $stmt->close();
+
+            return $user;
+        } else {
+            return false;
+        }
+    }
+
+    public function getUserByEmailAndPassword($email, $password) {
+
+        $stmt = $this->conn->prepare("SELECT * FROM users WHERE email = ?");
+
+        $stmt->bind_param("s", $email);
+
+        if ($stmt->execute()) {
+            $user = $stmt->get_result()->fetch_assoc();
+            $stmt->close();
+
+            // verifying user password
+            $salt = $user['salt'];
+            $encrypted_password = $user['encrypted_password'];
+            $hash = $this->checkhashSSHA($salt, $password);
+            // check for password equality
+            if ($encrypted_password == $hash) {
+                // user authentication details are correct
+                return $user;
+            }
+        } else {
+            return NULL;
+        }
+    }
+
+    public function isUserExisted($email) {
+        $stmt = $this->conn->prepare("SELECT email from users WHERE email = ?");
+
+        $stmt->bind_param("s", $email);
+
+        $stmt->execute();
+
+        $stmt->store_result();
+
+        if ($stmt->num_rows > 0) {
+            // user existed 
+            $stmt->close();
+            return true;
+        } else {
+            // user not existed
+            $stmt->close();
+            return false;
+        }
+    }
+
+    public function hashSSHA($password) {
+
+        $salt = sha1(rand());
+        $salt = substr($salt, 0, 10);
+        $encrypted = base64_encode(sha1($password . $salt, true) . $salt);
+        $hash = array("salt" => $salt, "encrypted" => $encrypted);
+        return $hash;
+    }
+
+    public function checkhashSSHA($salt, $password) {
+
+        $hash = base64_encode(sha1($password . $salt, true) . $salt);
+
+        return $hash;
+    }
+
+}
+
+?>
